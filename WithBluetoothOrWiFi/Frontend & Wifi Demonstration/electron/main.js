fd77ab28618645cdaa9cb5e2cd89b2e91821309c
Yacob Ahmad <128879964+CoobTheScientist@users.noreply.github.com>
2025-03-11 16:07:39 -0500

    Update main.js

--- WithBluetoothOrWiFi/Frontend & Wifi Demonstration/electron/main.js
+++ WithBluetoothOrWiFi/Frontend & Wifi Demonstration/electron/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 1000,
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js')
@@ -41,5 +41,3 @@ app.on('window-all-closed', function () {
   app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
